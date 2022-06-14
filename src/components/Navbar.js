3f72a246d485f92b65ebe824698cbb2697f809e3
DebasmitBiswal <debasmitbiswal@outlook.com>
2022-06-14 22:38:53 +0530

    Done with the NavBar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,20 @@ import { Link } from "react-router-dom";
 import { Stack } from "@mui/material";
 
 import Logo from "../assets/images/Logo.png";
+import { borderBottom } from "@mui/system";
 
 const Navbar = () => {
   return (
-    <Stack>
+    <Stack
+      direction="row"
+      justifyContent="space-around"
+      sx={{
+        gap: { sm: "122px", xs: "40px" },
+        mt: { sm: "32px", xs: "20px" },
+        justifyContent: "none",
+      }}
+      px="20px"
+    >
       <Link to="/">
         <img
           src={Logo}
@@ -14,6 +24,24 @@ const Navbar = () => {
           style={{ wisth: "48px", height: "48px", margin: "0 20px" }}
         />
       </Link>
+      <Stack direction="row" gap="40px" fontSize="24px" alignItem="flex-end">
+        <Link
+          to="/"
+          style={{
+            textdecoration: "none",
+            color: "#3A1212",
+            borderBottom: "3px solid #FF2625",
+          }}
+        >
+          Home
+        </Link>
+        <a
+          href="#exercises"
+          style={{ textDecoration: "none", color: "#3A1212" }}
+        >
+          Exercises
+        </a>
+      </Stack>
     </Stack>
   );
 };
