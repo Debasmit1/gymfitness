6d59170bc50bfa69b3be50cebb37b3f9a0441e36
DebasmitBiswal <debasmitbiswal@outlook.com>
2022-06-14 20:07:40 +0530

    Added the logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Stack } from "@mui/material";
+
+import Logo from "../assets/images/Logo.png";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Stack>
+      <Link to="/">
+        <img
+          src={Logo}
+          alt="logo"
+          style={{ wisth: "48px", height: "48px", margin: "0 20px" }}
+        />
+      </Link>
+    </Stack>
+  );
 };
 
 export default Navbar;
