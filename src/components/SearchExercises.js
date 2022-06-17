0690b71fc2c01ed670605df11b3a79548322240f
DebasmitBiswal <debasmitbiswal@outlook.com>
2022-06-17 20:12:23 +0530

    Done with Adding Section

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -3,9 +3,8 @@ import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 import { exerciseOptions, fetchData } from "../utils/fetchData";
 import HorizontalScrollbar from "./HorizontalScrollbar";
 
-const SearchExercises = () => {
+const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
   const [search, setSearch] = useState("");
-  const [exercises, setExercises] = useState([]);
   const [bodyParts, setBodyParts] = useState([]);
 
   useEffect(() => {
@@ -17,6 +16,8 @@ const SearchExercises = () => {
 
       setBodyParts(["all", ...bodyPartsData]);
     };
+
+    fetchExercisesData();
   }, []);
 
   const handleSearch = async () => {
@@ -85,7 +86,11 @@ const SearchExercises = () => {
         </Button>
       </Box>
       <Box sx={{ position: "relative", width: "100%", p: "20px" }}>
-        <HorizontalScrollbar data={bodyParts} />
+        <HorizontalScrollbar
+          data={bodyParts}
+          bodyPart={bodyPart}
+          setBodyPart={setBodyPart}
+        />
       </Box>
     </Stack>
   );
