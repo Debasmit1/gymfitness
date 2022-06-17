78f1621af2b41482d1d5a4486fcecda76dd41509
DebasmitBiswal <debasmitbiswal@outlook.com>
2022-06-18 03:27:48 +0530

    Done with the homepage

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -6,6 +6,42 @@ import ExerciseCard from "./ExerciseCard";
 
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
   // console.log(exercises);
+  const [currentPage, setCurrentPage] = useState(1);
+  const exercisesPerPage = 9;
+  const indexOfLastExercise = currentPage * exercisesPerPage;
+  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
+  const currentExercise = exercises.slice(
+    indexOfFirstExercise,
+    indexOfLastExercise
+  );
+
+  const paginate = (e, value) => {
+    setCurrentPage(value);
+
+    window.scrollTo({ top: 1800, behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      let exercisesData = [];
+
+      if (bodyPart === "all") {
+        exercisesData = await fetchData(
+          "https://exercisedb.p.rapidapi.com/exercises",
+          exerciseOptions
+        );
+      } else {
+        exercisesData = await fetchData(
+          `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`,
+          exerciseOptions
+        );
+      }
+
+      setExercises(exercisesData);
+    };
+    fetchExercisesData();
+  }, [bodyPart]);
+
   return (
     <div>
       <Box id="exercises" sx={{ mt: { lg: "110px" } }} mt="50px" p="20px">
@@ -18,9 +54,22 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
           flexWrap="wrap"
           justifyContent="center"
         >
-          {exercises.map((exercise, index) => (
+          {currentExercise.map((exercise, index) => (
             <ExerciseCard id={index} exercise={exercise} />
           ))}
+        </Stack>
+        <Stack mt="100px" alignItems="center">
+          {exercises.length > 9 && (
+            <Pagination
+              color="standard"
+              shape="rounded"
+              defaultPage={1}
+              count={Math.ceil(exercises.length / exercisesPerPage)}
+              page={currentPage}
+              onChange={paginate}
+              size="large"
+            />
+          )}
         </Stack>
       </Box>
     </div>
